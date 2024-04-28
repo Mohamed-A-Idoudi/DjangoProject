4db47619280283e3d014091a9cc8f3a1fcedaed8
Mohamed Idoudi <idoudiamine83@gmail.com>
2024-04-28 22:52:22 +0100

    final project

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -69,7 +69,8 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
-        'PORT': '3306'
+        'PORT': '3306',
+        'OPTIONS': {'sql_mode': 'traditional',}
     }
 }
 
